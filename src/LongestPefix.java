d5650d637d2a79119fdbd28a095a99ce2135a321
Resul Huseynli <resulhuseynli4@gmail.com>
2025-04-24 18:37:38 +0400

    All Done!

---
+++ src/LongestPefix.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class LongestPefix {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length - 1];
+        int idx = 0;
+        while (idx < s1.length() && idx < s2.length()) {
+            if (s1.charAt(idx) == s2.charAt(idx)) {
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+
+    public static void main(String[] args) {
+        LongestPefix longestPefix = new LongestPefix();
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(longestPefix.longestCommonPrefix(strs)); // Output: "fl"
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-
-class Solution {
+class Palindrome {
     public boolean isPalindrome(int x) {
         if (x < 0) {
             return false;
@@ -16,31 +14,13 @@ class Solution {
         return (reversed == x);
     }
 
-    public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String s1 = strs[0];
-        String s2 = strs[strs.length - 1];
-        int idx = 0;
-        while (idx < s1.length() && idx < s2.length()) {
-            if (s1.charAt(idx) == s2.charAt(idx)) {
-                idx++;
-            } else {
-                break;
-            }
+    public static class Main {
+        public static void main(String[] args) {
+
+            Palindrome solution = new Palindrome();
+            System.out.println(solution.isPalindrome(121));
+            System.out.println(solution.isPalindrome(-121));
+
         }
-        return s1.substring(0, idx);
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-
-        Solution solution = new Solution();
-        System.out.println(solution.isPalindrome(121));
-        System.out.println(solution.isPalindrome(-121));
-
-        String[] strs = {"flower", "flow", "flight"};
-        System.out.println(solution.longestCommonPrefix(strs));
-
     }
 }
